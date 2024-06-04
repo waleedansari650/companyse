b5a05bde8644aae32b43acf2a332daa2eb65a13a
waleed <waleedansari650@gmail.com>
2024-06-04 14:57:23 +0500

    first commit

---
+++ backend/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+import { json } from 'body-parser';
+
+
+
+
+
+
---
+++ interface/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h3>Waleed Bin Fazal</h3>
+      
+    </>
+  )
+}
+
+export default App
